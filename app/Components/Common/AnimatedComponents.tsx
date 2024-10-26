1d02597c5b67d1d900da637983c02285b51a90e7
six-standard <dbrrl1127@gmail.com>
2024-10-26 19:08:57 +0900

    chore: TouchableWithoutFeedback 추가

--- app/Components/Common/AnimatedComponents.tsx
+++ app/Components/Common/AnimatedComponents.tsx
@@ -1,11 +1,17 @@
 import { Path as NativePath, Rect as NativeRect, Circle as NativeCircle } from "react-native-svg";
-import { Animated, TouchableOpacity as NativeTouchable, TextInput } from "react-native";
-import { ScrollView as NativeScroll } from "react-native-gesture-handler";
+import { Animated, TextInput, TouchableOpacity as NativeTouchable } from "react-native";
+import {
+  ScrollView as NativeScroll,
+  TouchableWithoutFeedback as NativeTouchableWithoutFeedback,
+} from "react-native-gesture-handler";
 
 export const View = Animated.View;
 export const NativeText = Animated.Text;
 export const ScrollView = Animated.createAnimatedComponent(NativeScroll);
 export const TouchableOpacity = Animated.createAnimatedComponent(NativeTouchable);
+export const TouchableWithoutFeedback = Animated.createAnimatedComponent(
+  NativeTouchableWithoutFeedback
+);
 export const NativeInput = Animated.createAnimatedComponent(TextInput);
 export const Path = Animated.createAnimatedComponent(NativePath);
 export const Rect = Animated.createAnimatedComponent(NativeRect);

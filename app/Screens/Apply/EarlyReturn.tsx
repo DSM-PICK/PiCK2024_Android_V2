2f1c30ee01691bddeef422c21b9bb419de625b29
1408bg <bgyang1220@gmail.com>
2025-12-26 17:24:01 +0900

    feat: 조기귀가에서 교시 단위 신청 활성화

--- app/Screens/Apply/EarlyReturn.tsx
+++ app/Screens/Apply/EarlyReturn.tsx
@@ -1,17 +1,27 @@
-import { useMyMutation, useToast } from "@/hooks";
+import { useBottomSheet, useMyMutation, useOptions, useTheme, useToast } from "@/hooks";
 import { IEarlyReturnIn } from "@/apis";
 import { useState } from "react";
-import { Button, LabelLayout, Layout, PrevHeader, Text, TextInput, KeyboardDismiss, TimePickerButton } from "@/Components";
+import { Button, changePropType, LabelLayout, Layout, PrevHeader, Text, TextInput, KeyboardDismiss, TimePickerButton, TouchableOpacity, TimePicker } from "@/Components";
 import { StyleSheet } from "react-native";
 
 export const EarlyReturn = ({ navigation }) => {
   const { mutate: outMutate } = useMyMutation<IEarlyReturnIn, null>("post", "earlyReturn", "/create");
+  const { periodType } = useOptions();
+  const { open } = useBottomSheet();
   const { success, error } = useToast();
+  const { color } = useTheme();
 
   const [data, setData] = useState<IEarlyReturnIn>({
     reason: "",
     start: "",
   });
+
+  const handleChange = (e: changePropType) => {
+    setData({
+      ...data,
+      start: `${e.hour.padStart(2, "0")}:${e.minute.padStart(2, "0")}`,
+    });
+  };
 
   return (
     <KeyboardDismiss>
@@ -19,17 +29,61 @@ export const EarlyReturn = ({ navigation }) => {
         <Text colorType="normal" colorLevel="black" fontType="heading" fontLevel={4}>
           조기 귀가 신청
         </Text>
-        <LabelLayout label="희망 귀가 시간을 선택하세요" type="black">
-          <TimePickerButton title="귀가 시간을 선택하세요" type="time" value={data.start} onChange={(e) => setData({ ...data, start: `${e.hour.padStart(2, "0")}:${e.minute.padStart(2, "0")}` })} />
+        <LabelLayout label={`희망 귀가 ${!!periodType ? "교시를" : "시간을"} 선택하세요`} type="black">
+          {periodType === 0 ? (
+            <TimePickerButton 
+              title={`귀가 ${!!periodType ? "교시를" : "시간을"} 선택하세요`} 
+              type={!!periodType ? "class" : "time"} 
+              value={data.start} 
+              onChange={handleChange} 
+            />
+          ) : (
+            <TouchableOpacity
+              style={{
+                ...styles.classPickerContainer,
+                backgroundColor: color("gray", 50),
+                justifyContent: data.start ? "center" : "flex-start",
+              }}
+              activeOpacity={1}
+              onPress={() => 
+                open(
+                  <TimePicker 
+                    title="귀가 교시를 선택해주세요" 
+                    buttonTitle="선택 완료" 
+                    type="class" 
+                    onEnd={(e) => setData({ ...data, start: e.hour + "교시" })} 
+                  />
+                )
+              }
+            >
+              {data.start ? (
+                <Text colorType="normal" colorLevel="black" fontType="body" fontLevel={1}>
+                  {data.start}
+                </Text>
+              ) : (
+                <Text colorType="gray" colorLevel={500} fontType="body" fontLevel={1}>
+                  선택
+                </Text>
+              )}
+            </TouchableOpacity>
+          )}
         </LabelLayout>
         <LabelLayout label="귀가 사유를 입력하세요" type="black">
-          <TextInput value={data.reason} onChange={(e) => setData({ ...data, reason: e })} multiLine={6} placeholder="자세히 입력해주세요" />
+          <TextInput 
+            value={data.reason} 
+            onChange={(e) => setData({ ...data, reason: e })} 
+            multiLine={6} 
+            placeholder="자세히 입력해주세요" 
+          />
         </LabelLayout>
         <Button
           style={styles.button}
           onPress={() =>
             outMutate(
-              { ...data, start: data.start + ":00" },
+              { 
+                ...data, 
+                start: periodType === 0 ? data.start + ":00" : data.start 
+              },
               {
                 onSuccess: () => success("조기 귀가 신청이 완료되었습니다!"),
                 onError: (err) => {
@@ -55,5 +109,13 @@ const styles = StyleSheet.create({
     position: "absolute",
     bottom: 30,
     alignSelf: "center",
+  },
+  classPickerContainer: {
+    width: "100%",
+    height: 40,
+    paddingHorizontal: 15,
+    flexDirection: "row",
+    borderRadius: 8,
+    alignItems: "center",
   },
 });
